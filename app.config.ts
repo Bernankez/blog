d88f8ea472811470610358781ca23107e864d86b
cole <bernankez@qq.com>
2023-03-07 17:22:26 +0800

    chore: update basic configs

--- app.config.ts
+++ app.config.ts
@@ -1,11 +1,18 @@
 export default defineAppConfig({
   docus: {
-    title: "Docus",
-    description: "The best place to start your documentation.",
-    image: "https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png",
+    title: "科科Cole",
+    titleTemplate: "%s",
+    description: "Cole.blog",
+    url: "https://blog.keke.cc",
+    // TODO social card preview
+    // image: "https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png",
     socials: {
-      twitter: "nuxtstudio",
-      github: "nuxt-themes/docus",
+      "github": "bernankez",
+      "keke.cc": {
+        label: "keke.cc",
+        icon: "tabler:brand-redhat",
+        href: "https://keke.cc",
+      },
     },
     aside: {
       level: 0,
@@ -15,12 +22,14 @@ export default defineAppConfig({
       logo: true,
     },
     footer: {
-      iconLinks: [
-        {
-          href: "https://nuxt.com",
-          icon: "simple-icons:nuxtdotjs",
-        },
-      ],
+      credits: false,
+    },
+    github: {
+      dir: "content",
+      branch: "master",
+      repo: "blog",
+      owner: "bernankez",
+      edit: true,
     },
   },
 });
---
+++ components/Logo.vue
@@ -0,0 +1,25 @@
+<template>
+  <img class="logo" src="/logo.png" alt="logo" />
+  <div class="title">
+    科科Cole
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+.logo {
+  width: 50px;
+  height: 50px;
+  margin-right: 10px;
+}
+
+.title {
+  font-size: 1.25rem;
+  font-weight: bold;
+  color: #18181B;
+  font-family: ui-sans-serif;
+}
+</style>
