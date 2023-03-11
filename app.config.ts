46125b92f673ba3687ddc337d06a9c68f1e1c96c
cole <bernankez@qq.com>
2023-03-11 19:56:04 +0800

    feat: overwrite DocsPageLayout

--- app.config.ts
+++ app.config.ts
@@ -4,6 +4,7 @@ export default defineAppConfig({
     description: "Cole.blog",
     url: "https://blog.keke.cc",
     layout: "default",
+    comment: true,
     // TODO social card preview
     // image: "https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png",
     socials: {
