7b86c2ebb206b9ffa1e4eacab184d9a38aa3dd74
cole <bernankez@qq.com>
2023-05-05 18:06:24 +0800

    wip(list): collections

---
+++ components/custom/CollectionItem.vue
@@ -0,0 +1,17 @@
+<template>
+  <NuxtLink class="flex flex-gap-2 p-3 rounded-3 box-border bg-yellow">
+    <img class="h-13 w-13 object-cover rounded-3 bg-gray" alt="logo" />
+    <div class="flex flex-col justify-between">
+      <div class="text-4">
+        Vuejs Org dfdffdf
+      </div>
+      <div class="text-3.5">
+        desc
+      </div>
+    </div>
+  </NuxtLink>
+</template>
+
+<script setup lang="ts">
+
+</script>
