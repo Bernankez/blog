cfef893cf88cbd53266d50726a6c3a1d9c9b7636
cole <bernankez@qq.com>
2023-05-06 01:48:20 +0800

    docs: collections

--- components/custom/CollectionItem.vue
+++ components/custom/CollectionItem.vue
@@ -1,17 +1,31 @@
 <template>
-  <NuxtLink class="flex flex-gap-2 p-3 rounded-3 box-border bg-yellow">
-    <img class="h-13 w-13 object-cover rounded-3 bg-gray" alt="logo" />
-    <div class="flex flex-col justify-between">
-      <div class="text-4">
-        Vuejs Org dfdffdf
-      </div>
-      <div class="text-3.5">
-        desc
+  <div class="flex flex-gap-2 p-3 rounded-3 box-border backdrop-blur cursor-default hover:bg-gray-50 dark:hover:bg-dark-800 transition-200">
+    <div class="shrink-0 flex justify-center items-center h-13 w-13 rounded-3">
+      <slot name="icon">
+        <div class="text-8 sm:text-9" :class="[icon]"></div>
+      </slot>
+    </div>
+    <div class="flex flex-col">
+      <NuxtLink :href="href" target="_blank" class="text-4 sm:text-5 underline underline-offset-4">
+        {{ name }}
+      </NuxtLink>
+      <div class="text-3.5 sm:text-4 m-t-2">
+        {{ description }}
       </div>
     </div>
-  </NuxtLink>
+  </div>
 </template>
 
 <script setup lang="ts">
-
+withDefaults(defineProps<{
+  icon?: string;
+  name?: string;
+  href?: string;
+  description?: string;
+}>(), {
+  icon: "",
+  name: "",
+  href: "",
+  description: "",
+});
 </script>
