c12935e8f7ba7be772cf945347c47c29faf6d68b
cole <bernankeic@gmail.com>
2023-12-05 16:42:00 +0800

    feat(Collapse): add title and desc prop

--- components/custom/Collapse.vue
+++ components/custom/Collapse.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="collapse grid overflow-hidden rounded-2 bg-gray-50 dark:bg-dark-800" :class="[collapsed ? 'p-b-0 grid-rows-[auto_0fr]' : 'p-b-5 grid-rows-[auto_1fr]']">
     <div class="z-1 flex cursor-pointer justify-between p-5" @click="collapsed = !collapsed">
-      <ContentSlot :use="$slots.summary" unwrap="p" />
+      <div>
+        <ContentSlot :use="$slots.summary" unwrap="p">
+          {{ title }}
+          <span class="m-l-2 align-end text-3.5 text-nuxt-gray-400">{{ desc }}</span>
+        </ContentSlot>
+      </div>
       <div class="i-icon-park-outline:right text-5 transition" :class="[collapsed ? '' : 'rotate-90']"></div>
     </div>
     <div class="content p-x-5" :class="[collapsed ? 'opacity-0 min-h-0' : 'min-h-fit']">
@@ -11,6 +16,11 @@
 </template>
 
 <script setup lang="ts">
+defineProps<{
+  title?: string;
+  desc?: string;
+}>();
+
 const collapsed = ref(false);
 </script>
 
